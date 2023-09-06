7d63117978cf24410bde9c6cc240cb8cd6930f88
Royyfzeng <royyfzeng@gmail.com>
2023-09-06 10:07:03 -0400

    Update Java
    - in ./shapes
    - all class and the shape interface are kept as public
    - all class variables are set as private
    - added copy constructor to every class to make defensive copies
    - In the renderer, added judgement for class type and exception handler
    - renderer only needs only need shape reference (pointer) to perform its function
    - encapsulation finished

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -2,10 +2,11 @@ package edu.cmu.cs.cs214.lab02;
 
 // import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 10);
+        Shape rectangle = new Rectangle(2, 10);
         Renderer renderer = new Renderer(rectangle);
         renderer.draw();
     }
