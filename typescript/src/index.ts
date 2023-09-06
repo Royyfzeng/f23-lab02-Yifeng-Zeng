8b7edba51c7d8d0c019cbcc1f5333c1740705097
Royyfzeng <royyfzeng@gmail.com>
2023-09-06 10:11:14 -0400

    Update to JavaScript
    - shape.ts export whole interface
    - circle.ts, square.ts, rectangle.ts  implements shape interface in inheritance manner, made all class variables private, made all class modifier as default.
    - renderer.ts accepted shape reference as parameter.
    - implemented encapsulation and hide information in an OOP manner.

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,6 @@
 import newRenderer from "./renderer"
 import Rectangle from "./shapes/rectangle";
 import Shape from "./shapes/shape";
-const rectangle: Shape = new Rectangle(2, 3)
-const renderer = newRenderer(rectangle)
+let rectangle: Shape = new Rectangle(2, 3)
+let renderer = newRenderer(rectangle)
 renderer.draw();
